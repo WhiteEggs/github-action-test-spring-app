87f665bc3732da9b1ebfa97cd26f1d05edc0cbe7
Joshua <yceo3756@gmail.com>
2025-09-08 16:52:16 +0800

    新增/check

--- src/main/java/com/example/controller/MyController.java
+++ src/main/java/com/example/controller/MyController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +13,12 @@ public class MyController {
     @RequestMapping("/hello")
     public ModelAndView sayHello() {
         String message = "Success";
-        return new ModelAndView("home", Map.of("message", message, "updatedAt", "2025/09/08 11:24"));
+        return new ModelAndView("home", Map.of("message", message, "updatedAt", "2025/09/08 16:41"));
+    }
+    
+    @RequestMapping("/check")
+    public ResponseEntity<String> check() {
+        String message = "Success";
+        return ResponseEntity.ok(message);
     }
 }
