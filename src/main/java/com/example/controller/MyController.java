e3111d50f061e176bab2a11ec024d21c8ad564aa
Joshua <yceo3756@gmail.com>
2025-09-09 15:41:52 +0800

    新增 /echo

--- src/main/java/com/example/controller/MyController.java
+++ src/main/java/com/example/controller/MyController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,12 +14,17 @@ public class MyController {
     @RequestMapping("/hello")
     public ModelAndView sayHello() {
         String message = "Success";
-        return new ModelAndView("home", Map.of("message", message, "updatedAt", "2025/09/08 16:41"));
+        return new ModelAndView("home", Map.of("message", message, "updatedAt", "2025/09/09 15:38"));
     }
     
     @RequestMapping("/check")
     public ResponseEntity<String> check() {
         String message = "Success";
+        return ResponseEntity.ok(message);
+    }
+    
+    @RequestMapping("/echo/{message}")
+    public ResponseEntity<String> echo(@PathVariable String message) {
         return ResponseEntity.ok(message);
     }
 }
